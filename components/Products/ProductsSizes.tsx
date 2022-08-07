95f4621dfa1bbfb8dca5616537346bbdf6ccb086
Andrew <growler625@gmail.com>
2022-08-07 09:22:54 +0300

    tsx fix

--- components/Products/ProductsSizes.tsx
+++ components/Products/ProductsSizes.tsx
@@ -9,8 +9,8 @@ const ProductsSizes = ({ sizes, onClick, currentSize }: ProductsSizesProps) => {
         <div className="product-sizes">
             <div className="product-sizes__label">SIZE</div>
             <div className="product-sizes__items">
-                {!!sizes && sizes.length && sizes.map((size) =>
-                    <div className={`product-sizes__item${currentSize == size ? " active" : ""}`} onClick={() => onClick(size)}>{size}</div>
+                {!!sizes && sizes.length && sizes.map((size, index) =>
+                    <div className={`product-sizes__item${currentSize == size ? " active" : ""}`} onClick={() => onClick(size)} key={index}>{size}</div>
                 )}
             </div>
         </div>
